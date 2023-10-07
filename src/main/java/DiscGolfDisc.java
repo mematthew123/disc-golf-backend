a52683582724fcbc322e68b9f8dc0ac40d1cbdeb
Matthew Rhoads <78577112+mematthew123@users.noreply.github.com>
2023-10-07 09:03:37 -0600

    Initial Setup with Model

---
+++ src/main/java/DiscGolfDisc.java
@@ -0,0 +1,76 @@
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class DiscGolfDisc {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String brand;
+    private String type; // Example: Driver, Mid-range, Putter
+
+    // Default constructor
+    public DiscGolfDisc() {
+    }
+
+
+    // Constructor
+    public DiscGolfDisc(String name, String brand, String type) {
+        this.name = name;
+        this.brand = brand;
+        this.type = type;
+    }
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+
+    public String getType() {
+        return type;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+
+    // toString method
+    @Override
+    public String toString() {
+        return "DiscGolfDisc{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", brand='" + brand + '\'' +
+                ", type='" + type + '\'' +
+                '}';
+    }
+}
+
+
+
